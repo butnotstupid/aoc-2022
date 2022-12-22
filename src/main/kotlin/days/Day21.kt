fa4abba7d441b0c2c49ab7dc79cb1bf30a425efb
butnotstupid <butnotstupid@gmail.com>
2022-12-22 21:45:04 +0100

    Day21**

--- src/main/kotlin/days/Day21.kt
+++ src/main/kotlin/days/Day21.kt
@@ -1,11 +1,73 @@
 package days
 
+typealias Operation = (Day21.LinearFunction, Day21.LinearFunction) -> Day21.LinearFunction
+
 class Day21 : Day(21) {
+
     override fun partOne(): Any {
-        TODO()
+        val monkeys = parseMonkeys().associateBy { it.name }
+        return monkeys.getValue("root").eval(monkeys).b.toLong()
     }
 
     override fun partTwo(): Any {
-        TODO()
+        val monkeys = parseMonkeys()
+            .map { if (it.name == "humn") it.copy(value = LinearFunction(1.0, 0.0)) else it }
+            .associateBy { it.name }
+
+        val (left, right) = monkeys.getValue("root").expr!!
+            .let { expr ->
+                monkeys.getValue(expr.left).eval(monkeys) to
+                monkeys.getValue(expr.right).eval(monkeys)
+            }
+
+        val x = (right.b - left.b) / left.a
+        return x.toLong()
     }
+
+    private fun parseMonkeys() = inputList.map {
+        it.split(": ").let { (key, value) ->
+            if (value.all(Char::isDigit)) Monkey(key, value.toLong().toLinearFunction())
+            else {
+                value.split(" ").let { (left, op, right) ->
+                    val operation: Operation = when (op.first()) {
+                        '+' -> { a, b -> a + b }
+                        '-' -> { a, b -> a - b }
+                        '*' -> { a, b -> a * b }
+                        '/' -> { a, b -> a / b }
+                        else -> throw IllegalArgumentException("Unknown operation: ${op.first()}")
+                    }
+                    Monkey(key, null, Expression(left, right, operation))
+                }
+            }
+        }
+    }
+
+    data class Monkey(val name: String, var value: LinearFunction? = null, val expr: Expression? = null) {
+        fun eval(others: Map<String, Monkey>): LinearFunction {
+            if (value == null) {
+                with (expr!!) {
+                    value = operation(others.getValue(left).eval(others),others.getValue(right).eval(others))
+                }
+            }
+            return value!!
+        }
+    }
+    data class Expression(val left: String, val right: String, val operation: Operation)
+
+    // Linear function: ax + b
+    data class LinearFunction(val a: Double, val b: Double) {
+        operator fun plus(other: LinearFunction) = LinearFunction(this.a + other.a, this.b + other.b)
+        operator fun minus(other: LinearFunction) = LinearFunction(this.a - other.a, this.b - other.b)
+        operator fun times(other: LinearFunction): LinearFunction {
+            assert(this.a.compareTo(0) == 0 || other.a.compareTo(0) == 0) { "$this * ${other}: function should stay linear, otherwise not supported" }
+            return LinearFunction(
+                this.a * other.b + this.b * other.a, this.b * other.b
+            )
+        }
+        operator fun div(other: LinearFunction): LinearFunction {
+            assert(other.a.compareTo(0) == 0) { "$this / ${other}: function should stay linear, otherwise not supported" }
+            return LinearFunction(this.a / other.b, this.b / other.b)
+        }
+    }
+    private fun Long.toLinearFunction() = LinearFunction(0.0, this.toDouble())
 }
--- src/test/kotlin/days/Day21Test.kt
+++ src/test/kotlin/days/Day21Test.kt
@@ -10,15 +10,15 @@ class Day21Test {
 
     @Test
     fun testPartOne() {
-//        val partOne = dayTwentyOne.partOne()
-//        println("\nPART ONE: $partOne")
-//        assertThat(partOne, `is`())
+        val partOne = dayTwentyOne.partOne()
+        println("\nPART ONE: $partOne")
+        assertThat(partOne, `is`(87457751482938L))
     }
 
     @Test
     fun testPartTwo() {
-//        val partTwo = dayTwentyOne.partTwo()
-//        println("PART TWO: $partTwo\n")
-//        assertThat(partTwo, `is`())
+        val partTwo = dayTwentyOne.partTwo()
+        println("PART TWO: $partTwo\n")
+        assertThat(partTwo, `is`(3221245824363L))
     }
 }
