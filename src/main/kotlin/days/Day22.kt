86f986d9e844df7801f7f8faf1d2dddc2b5bc617
butnotstupid <butnotstupid@gmail.com>
2022-12-22 21:54:02 +0100

    Day22 prep

---
+++ src/main/kotlin/days/Day22.kt
@@ -0,0 +1,11 @@
+package days
+
+class Day22 : Day(22) {
+    override fun partOne(): Any {
+        TODO()
+    }
+
+    override fun partTwo(): Any {
+        TODO()
+    }
+}
---
+++ src/test/kotlin/days/Day22Test.kt
@@ -0,0 +1,24 @@
+package days
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.jupiter.api.Test
+
+class Day22Test {
+
+    private val dayTwentyTwo = Day22()
+
+    @Test
+    fun testPartOne() {
+//        val partOne = dayTwentyTwo.partOne()
+//        println("\nPART ONE: $partOne")
+//        assertThat(partOne, `is`())
+    }
+
+    @Test
+    fun testPartTwo() {
+//        val partTwo = dayTwentyTwo.partTwo()
+//        println("PART TWO: $partTwo\n")
+//        assertThat(partTwo, `is`())
+    }
+}
