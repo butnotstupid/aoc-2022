af2cb2a6a3091e74f62994b583377d7e940e1006
butnotstupid <butnotstupid@gmail.com>
2022-12-26 19:36:57 +0100

    Day25 prep

---
+++ src/test/kotlin/days/Day25Test.kt
@@ -0,0 +1,22 @@
+package days
+
+import org.junit.jupiter.api.Test
+
+class Day25Test {
+
+    private val dayTwentyFive = Day25()
+
+    @Test
+    fun testPartOne() {
+//        val partOne = dayTwentyFive.partOne()
+//        println("\nPART ONE: $partOne")
+//        assertThat(partOne, `is`())
+    }
+
+    @Test
+    fun testPartTwo() {
+//        val partTwo = dayTwentyFive.partTwo()
+//        println("PART TWO: $partTwo\n")
+//        assertThat(partTwo, `is`())
+    }
+}
